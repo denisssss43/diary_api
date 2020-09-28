a290deeeb390cbbe222fb851a45bd2b452352603
Denis <38821830+denisssss43@users.noreply.github.com>
2020-09-28 22:35:45 +0700

    ...

--- db/migrate/20200928152754_change_notes.rb
+++ db/migrate/20200928152754_change_notes.rb
@@ -1,9 +1,5 @@
 class ChangeNotes < ActiveRecord::Migration[6.0]
   def change
-    create_table :notes do |t|
-      t.text :text
-      t.references :diary
-      t.timestamps
-    end
+    add_column :notes , :diary_id, :integer
   end
 end
