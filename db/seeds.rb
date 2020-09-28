eefb0b8582404c95e055595d59fc8a459845a3db
Denis <38821830+denisssss43@users.noreply.github.com>
2020-09-28 22:12:31 +0700

    seeds update

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,21 @@
 
 
 d = Diary.create(title: "Diary 1", expiration: nil, kind: :is_public)
-d = Diary.create(title: "Diary 1", expiration: nil, kind: :is_private)
-d = Diary.create(title: "Diary 1", expiration: Time.now + 10.minutes, kind: :is_private)
-d = Diary.create(title: "Diary 1", expiration: Time.now + 10.minutes, kind: :is_private)
+d.notes.create(text: "diary_1-note_1: text...")
+d.notes.create(text: "diary_1-note_2: text...")
+d.notes.create(text: "diary_1-note_3: text...")
+
+d = Diary.create(title: "Diary 2", expiration: nil, kind: :is_private)
+d.notes.create(text: "diary_2-note_1: text...")
+d.notes.create(text: "diary_2-note_2: text...")
+d.notes.create(text: "diary_2-note_3: text...")
+
+d = Diary.create(title: "Diary 3", expiration: Time.now + 10.minutes, kind: :is_private)
+d.notes.create(text: "diary_3-note_1: text...")
+d.notes.create(text: "diary_3-note_2: text...")
+d.notes.create(text: "diary_3-note_3: text...")
+
+d = Diary.create(title: "Diary 4", expiration: Time.now + 10.minutes, kind: :is_private)
+d.notes.create(text: "diary_4-note_1: text...")
+d.notes.create(text: "diary_4-note_2: text...")
+d.notes.create(text: "diary_4-note_3: text...")
