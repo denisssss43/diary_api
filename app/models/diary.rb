f1cc4ab57e3b8dd257638f1eda528d4da7485b98
Denis <38821830+denisssss43@users.noreply.github.com>
2020-09-28 19:39:56 +0700

    Update diary model

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,2 +1,10 @@
 class Diary < ApplicationRecord
+    validates: :title, presence: true 
+    validates: :kind, presence: true
+    validates: :expiration, inclusion: { in: [nil]}, if: :is_public?
+
+
+    def is_public?
+        kind == 0
+    end 
 end
