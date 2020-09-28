9efece0e3b1107c568fb474e958f43b88398dc86
Denis <38821830+denisssss43@users.noreply.github.com>
2020-09-28 21:45:02 +0700

    ...

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,12 +1,12 @@
 class Diary < ApplicationRecord
     
-    enum kind: {public:0, private:1}, kind == :public
+    enum kind: {public:0, private:1}
     
     validates :title, presence: true 
     validates :kind, presence: true
     validates :expiration, inclusion: { in: [nil]}, if: :is_public?
 
     def is_public?
-        kind == 0
+        kind == :public
     end 
 end
