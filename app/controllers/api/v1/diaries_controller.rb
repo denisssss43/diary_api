30f733663e663632b76c98430b115589bb473eb2
Denis <38821830+denisssss43@users.noreply.github.com>
2020-09-29 15:25:26 +0700

    ...

--- app/controllers/api/v1/diaries_controller.rb
+++ app/controllers/api/v1/diaries_controller.rb
@@ -19,7 +19,7 @@ class Api::V1::DiariesController < ApplicationController
 
     def create
         diary = Diary.new(diary_params)
-        if diary.validated
+        if diary.save
             render json: diary, stasus: :ok
         else
             render json: diary.errors, stasus: :unprocessable_entity
