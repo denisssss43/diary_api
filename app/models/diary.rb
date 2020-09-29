4642b64d909ed13aa13007f33bfdfcc28487e717
Denis <38821830+denisssss43@users.noreply.github.com>
2020-09-29 15:44:28 +0700

    ...

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -6,9 +6,9 @@ class Diary < ApplicationRecord
     validates :kind, presence: true
     validates :expiration, inclusion: { in: [nil]}, if: :is_public?
 
-    # has_many :notes, dependent: :destroy
+    has_many :notes, dependent: :destroy
 
     def is_public?
-        kind == :is_public
+        return kind == :is_public
     end 
 end
