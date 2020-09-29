4e8a9e501e594366ac3985e3aabb4c89f0ae7890
Denis <38821830+denisssss43@users.noreply.github.com>
2020-09-29 14:03:44 +0700

    update model

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -3,8 +3,8 @@ class Diary < ApplicationRecord
     enum kind: {is_public:0, is_private:1}
     
     validates :title, presence: true 
-    validates :expiration, inclusion: { in: [nil]}, if: :is_public?
     validates :kind, presence: true
+    validates :expiration, inclusion: { in: [nil]}, if: :is_public?
 
     has_many :notes, dependent: :destroy
 
