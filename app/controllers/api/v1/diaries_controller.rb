1c1d34f74b5d98fad9e3c0d3759387b037cd8f09
Denis <38821830+denisssss43@users.noreply.github.com>
2020-09-29 11:20:44 +0700

    Update controllers render

--- app/controllers/api/v1/diaries_controller.rb
+++ app/controllers/api/v1/diaries_controller.rb
@@ -1,7 +1,17 @@
 class Api::V1::DiariesController < ApplicationController
-    before_action :set_diary
+    # before_action :set_diary
+
+    def index
+        @diaries = Diary.order('created_at DESC')
+        render json: {
+            status: 'SUCCESS', 
+            message: 'Loaded diaries',
+            data:@diaries
+        }, status: :ok
+    end
 
     def show
+        
     end
 
     private
