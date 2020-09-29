77e48e41d322b6cae96eebc70e6be2bb7959c736
Denis <38821830+denisssss43@users.noreply.github.com>
2020-09-29 11:29:17 +0700

    ...

--- app/controllers/api/v1/diaries_controller.rb
+++ app/controllers/api/v1/diaries_controller.rb
@@ -1,22 +1,12 @@
 class Api::V1::DiariesController < ApplicationController
-    # before_action :set_diary
 
     def index
         diaries = Diary.order('created_at DESC');
-        render json: {
-            status: 'SUCCESS', 
-            message: 'Loaded diaries',
-            data:diaries
-        }, status: :ok
+        render json: {status: 'SUCCESS', message: 'Loaded diaries', data:diaries}, status: :ok
     end
 
     def show
         
     end
-
-    private
-        def set_diary
-            @diary = Diary.find(params[:id])
-        end
 
 end
--- app/views/api/v1/diaries/show.json.jbuilder
+++
@@ -1,7 +0,0 @@
-json.id @diary.id
-json.title @diary.title
-json.expiration @diary.expiration
-json.kind @diary.kind
-json.notes @diary.notes do |note|
-    json.text note.text
-end
