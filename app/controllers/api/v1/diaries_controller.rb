91a74b2108dfaf0c46f7cc313f904cc0b1baa25a
Denis <38821830+denisssss43@users.noreply.github.com>
2020-09-29 22:55:25 +0700

    routes diary updates

--- app/controllers/api/v1/diaries_controller.rb
+++ app/controllers/api/v1/diaries_controller.rb
@@ -2,20 +2,37 @@ class Api::V1::DiariesController < ApplicationController
 
     # Список из 100 последних дневников
     def index
-        diaries = Diary.order('created_at DESC').take(100);
+        diaries = Diary.order('created_at DESC').take(100)
         render json: diaries, status: :ok
     end
 
     def show
         diary = Diary.find(params[:id])
-        render json: diary, stasus: :ok
+        render json: diary, stasus: :show
     end
 
     def create
         diary = Diary.new(diary_params)
 
         if diary.save
-            render json: diary, stasus: :ok
+            render json: diary, stasus: :create
+        else
+            render json: diary.errors, stasus: :unprocessable_entity
+        end 
+    end
+
+    def destroy
+        diaries = Diary.find(params[:id])
+        diaries.destroy
+
+        render json: diaries, status: :destroy
+    end
+
+    def update
+        diary = Diary.find(params[:id])
+
+        if diary.update_attributes(diary_params)
+            render json: diary, stasus: :update
         else
             render json: diary.errors, stasus: :unprocessable_entity
         end 
