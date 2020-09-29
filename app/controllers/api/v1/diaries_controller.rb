3731d811ead8dfaee163a422fc9c0b420957f31b
Denis <38821830+denisssss43@users.noreply.github.com>
2020-09-29 11:31:47 +0700

    ...

--- app/controllers/api/v1/diaries_controller.rb
+++ app/controllers/api/v1/diaries_controller.rb
@@ -2,7 +2,11 @@ class Api::V1::DiariesController < ApplicationController
 
     def index
         diaries = Diary.order('created_at DESC');
-        render json: {status: 'SUCCESS', message: 'Loaded diaries', data:diaries}, status: :ok
+        render json: {
+            status: 'SUCCESS', 
+            message: 'Loaded diaries', 
+            # data:diaries
+        }, status: :ok
     end
 
     def show
