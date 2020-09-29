5ebc72c7839c97ee7d31fa796be4e067a225db7d
Denis <38821830+denisssss43@users.noreply.github.com>
2020-09-29 10:41:32 +0700

    ...

--- app/controllers/api/v1/diaries_controller.rb
+++ app/controllers/api/v1/diaries_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::DiariesController < ApplicationController
 
     private
         def set_diary
-            @diary = Diary.find(params[:id])
+            @diary = Diary.last # .find(params[:id])
         end
 
 end
