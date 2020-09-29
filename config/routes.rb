f348df1d7fa797b68845689f4375b3b0a8a5b4a0
Denis <38821830+denisssss43@users.noreply.github.com>
2020-09-29 23:23:14 +0700

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
     namespace :api do
         namespace :v1 do
-            resources :diaries do
-                resources :notes
-            end
+            resources :diaries 
+            resources :notes
         end
     end
 end
