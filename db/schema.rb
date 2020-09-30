29de530ac5ee60eac60e6867c7170d866a66fe9b
rails <denisssss43>
2020-09-30 12:12:21 +0700

    ...

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_082949) do
+ActiveRecord::Schema.define(version: 2020_09_28_152754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_09_28_082949) do
     t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "diary_id"
   end
 
 end
