497fd4e9bc986138810bbb05026bd1833fa74aaa
Denis <38821830+denisssss43@users.noreply.github.com>
2020-09-30 09:43:10 +0700

    Add sidekiq routs to configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+    require 'sidekiq/web'
+    require 'sidekiq-scheduler/web'
+
     namespace :api do
         namespace :v1 do
             resources :diaries 
             resources :notes
         end
     end
+    
+    mount Sidekiq::Web, at: '/sidekiq'
+
 end
