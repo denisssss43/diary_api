5aa4b241b9fdeace84eca00896706a95725e7132
Denis <38821830+denisssss43@users.noreply.github.com>
2020-09-30 10:38:53 +0700

    Create sidekiq worker for drop diary

---
+++ app/workers/destroy_expiration_worker.rb
@@ -0,0 +1,13 @@
+require 'sidekiq-scheduler'
+
+class DestroyExpirationWorker  
+    include Sidekiq::Worker
+    sidekiq_options retry: false
+
+    def perform
+        diaries = Diary.where(["expiration < ?", Time.now])
+        diaries.each do |d| 
+            d.destroy
+    end
+
+end
